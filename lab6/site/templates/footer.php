6075c5725219f9d3585ae0501b6663af9969ca19
VNTU Student <vntu.student@users.noreply.github.com>
2026-09-24 21:25:24 +0300

    refactor: wrap template lines

--- lab6/site/templates/footer.php
+++ lab6/site/templates/footer.php
@@ -1,7 +1,8 @@
         </div>
     </div>
     <div id="footer">
-        <p>© 2026 Офіційний сайт туру «Океан Ельзи». <a href="https://okeanelzy.com">okeanelzy.com</a></p>
+        <p>© 2026 Офіційний сайт туру «Океан Ельзи».
+        <a href="https://okeanelzy.com">okeanelzy.com</a></p>
     </div>
 </body>
 </html>
